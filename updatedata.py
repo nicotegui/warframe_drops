06dd11497d17ad1f7d113b7378ad82c985012039
Nico R <143574499+nicotegui@users.noreply.github.com>
2023-12-17 17:56:45 -0500

    Create updatedata.py
    *COMPLETE*
    
    Parses the HTML content from the public warframe droptable into several csv files, organized by location, in order to make the data more understandable

---
+++ updatedata.py
@@ -0,0 +1,66 @@
+import os
+import requests
+import pandas as pd
+import dask.dataframe as dd
+from bs4 import BeautifulSoup
+import io
+import dask.diagnostics
+import time
+
+
+# Function to process each id
+def process_id(current_id):
+    # Find the HTML element with the current id
+    current_heading = soup.find('h3', {'id': current_id})
+
+    # Navigate to the table following the heading
+    table = current_heading.find_next('table')
+
+    # Create a Dask dataframe from the HTML table
+    html_table = str(table)
+    with dask.diagnostics.ProgressBar():
+        ddf = dd.from_pandas(pd.read_html(io.StringIO(html_table))[0], npartitions=1)
+
+    # Determine the CSV file path
+    csv_filename = f'raw_{current_id}.csv'
+
+    # Check if the CSV file already exists
+    if os.path.exists(csv_filename):
+        try:
+            # Remove the existing CSV file
+            os.remove(csv_filename)
+            print(f"\nExisting CSV file '{csv_filename}' has been deleted.")
+        except Exception as e:
+            print(f"\nError deleting existing CSV file '{csv_filename}': {e}")
+
+        # Write the entire dataframe to a new CSV file
+    try:
+        ddf.compute().to_csv(csv_filename, index=False)
+        print(f"\nCSV file '{csv_filename}' has been created.")
+    except Exception as e:
+        print(f"\nError writing CSV file '{csv_filename}': {e}")
+
+
+# Get URL
+url = 'https://www.warframe.com/droptables'
+response = requests.get(url)
+
+# Parse the HTML content using BS
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Find all <h3> elements with an "id" attribute
+h3_elements = soup.find_all('h3', {'id': True})
+
+# Extract the id values from the elements
+id_list = [element['id'] for element in h3_elements]
+
+# Start the timer
+start_time = time.time()
+
+# Process each id using Dask
+for current_id in id_list:
+    process_id(current_id)
+
+# Calculate and print the total time taken
+total_time = time.time() - start_time
+print(f"\nTotal time taken: {total_time:.2f} seconds.")
