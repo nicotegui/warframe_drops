56a71168b38fc94a115d271809dd1784a50aaf46
nico <nicolasreategui0@gmail.com>
2024-01-03 17:27:14 -0500

    More Updates Fixing the Code

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import pandas as pd
 
-# Read data from CSV file
-df = pd.read_csv('raw_modByAvatar.csv', nrows=7)
+
+file_path = 'raw_modByAvatar.csv'
+header = 'Mob,Mod,Chance\n'
+
+# Open CSV file
+with open('file_path', 'r') as file:
+    content = file.read()
+    first_line = file.readline()
+
+if first_line == header:
+    print("")
+else:
+    # Add the header row
+    with open(file_path, 'r') as file:
+        content = file.read()
+    
+    with open(file_path, 'w') as file:
+        file.write(header + content)
+
+# Read data from CSV file with pandas
+df = pd.read_csv('file_path')
 
 # Ask for mod to search for
 modsearch = input("Search Mod: ")
 print("Searching for " + modsearch)
+
 # Search for mod
+test = df[(df['Mod'] == 'The Sergeant')]
 
 # Display sources mod is dropped by and their percentages
 
-# Print list
+print("Printing Mods...\n" + test.head() + "\n Search Complete!")
 
-print("Search Complete")
